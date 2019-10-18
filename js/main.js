5b6f175bf9cd8b864b359701b7118c16d8904c40
ilataleksey <ilataleksey@gmail.com>
2019-10-18 07:29:32 +0300

    Исправляет ошибку с определением координат булавки, комментирует неиспользуемый код

--- js/main.js
+++ js/main.js
@@ -91,7 +91,7 @@ var mapActivation = function () {
   removeAttribute(adFormTextarea, 'disabled');
   removeAttribute(adFormButton, 'disabled');
   removeAttribute(mapFilterSelects, 'disabled');
-  inputAddress.value = leftCoordinate + mapMainPin.offsetWidth + MAP_MAIN_PIN_EDGE + ', ' + topCoordinate;
+  inputAddress.value = leftCoordinate + ', ' + (topCoordinate + Math.round(mapMainPin.offsetHeight / 2) + MAP_MAIN_PIN_EDGE);
 };
 
 var mapMainPin = document.querySelector('.map__pin--main');
@@ -138,199 +138,199 @@ adRoomNumber.addEventListener('change', function () {
 });
 
 
-var pinList = document.querySelector('.map__pins');
-var mapWidth = pinList.offsetWidth;
-var pinTemplate = document.querySelector('#pin')
-  .content
-  .querySelector('.map__pin');
+// var pinList = document.querySelector('.map__pins');
+// var mapWidth = pinList.offsetWidth;
+// var pinTemplate = document.querySelector('#pin')
+//   .content
+//   .querySelector('.map__pin');
 
-var getRandomInt = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+// var getRandomInt = function (min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min;
+// };
 
-var getNearStay = function () {
-  var accommodations = [];
-  for (var i = 0; i < stayCount; i++) {
-    var xLocation = getRandomInt(0, mapWidth) - 25;
-    var yLocation = getRandomInt(130, 630) - 35;
-    var accommodation = {
-      author: {
-        avatar: 'img/avatars/user0' + (i + 1) + '.png'
-      },
+// var getNearStay = function () {
+//   var accommodations = [];
+//   for (var i = 0; i < stayCount; i++) {
+//     var xLocation = getRandomInt(0, mapWidth) - 25;
+//     var yLocation = getRandomInt(130, 630) - 35;
+//     var accommodation = {
+//       author: {
+//         avatar: 'img/avatars/user0' + (i + 1) + '.png'
+//       },
 
-      offer: {
-        title: titles[i],
-        address: xLocation + ', ' + yLocation,
-        price: prices[i],
-        type: types[i],
-        rooms: rooms[i],
-        guests: guests[i],
-        checkin: checkins[i],
-        checkout: checkouts[i],
-        features: features[i],
-        description: descriptions[i],
-        photos: photos[i]
-      },
+//       offer: {
+//         title: titles[i],
+//         address: xLocation + ', ' + yLocation,
+//         price: prices[i],
+//         type: types[i],
+//         rooms: rooms[i],
+//         guests: guests[i],
+//         checkin: checkins[i],
+//         checkout: checkouts[i],
+//         features: features[i],
+//         description: descriptions[i],
+//         photos: photos[i]
+//       },
 
-      location: {
-        x: xLocation + 'px',
-        y: yLocation + 'px'
-      }
-    };
-    accommodations.push(accommodation);
-  }
-  return accommodations;
-};
+//       location: {
+//         x: xLocation + 'px',
+//         y: yLocation + 'px'
+//       }
+//     };
+//     accommodations.push(accommodation);
+//   }
+//   return accommodations;
+// };
 
-var accommodations = getNearStay();
+// var accommodations = getNearStay();
 
-var renderPin = function (accommodation) {
-  var pinElement = pinTemplate.cloneNode(true);
+// var renderPin = function (accommodation) {
+//   var pinElement = pinTemplate.cloneNode(true);
 
-  pinElement.style.left = accommodation.location.x;
-  pinElement.style.top = accommodation.location.y;
-  pinElement.querySelector('img').src = accommodation.author.avatar;
-  pinElement.querySelector('img').alt = accommodation.offer.title;
+//   pinElement.style.left = accommodation.location.x;
+//   pinElement.style.top = accommodation.location.y;
+//   pinElement.querySelector('img').src = accommodation.author.avatar;
+//   pinElement.querySelector('img').alt = accommodation.offer.title;
 
-  return pinElement;
-};
+//   return pinElement;
+// };
 
-var getPinList = function (arr) {
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < arr.length; i++) {
-    fragment.appendChild(renderPin(arr[i]));
-  }
-  return fragment;
-};
+// var getPinList = function (arr) {
+//   var fragment = document.createDocumentFragment();
+//   for (var i = 0; i < arr.length; i++) {
+//     fragment.appendChild(renderPin(arr[i]));
+//   }
+//   return fragment;
+// };
 
-var getOfferType = function (accommodation) {
-  switch (accommodation.offer.type) {
-    case 'flat':
-      var offerType = 'Квартира';
-      break;
-    case 'bungalo':
-      offerType = 'Бунгало';
-      break;
-    case 'house':
-      offerType = 'Дом';
-      break;
-    case 'palace':
-      offerType = 'Дворец';
-      break;
-  }
-  return offerType;
-};
+// var getOfferType = function (accommodation) {
+//   switch (accommodation.offer.type) {
+//     case 'flat':
+//       var offerType = 'Квартира';
+//       break;
+//     case 'bungalo':
+//       offerType = 'Бунгало';
+//       break;
+//     case 'house':
+//       offerType = 'Дом';
+//       break;
+//     case 'palace':
+//       offerType = 'Дворец';
+//       break;
+//   }
+//   return offerType;
+// };
 
-var getRoomsCount = function (accommodation) {
-  if (accommodation.offer.rooms === 1) {
-    var textRoomsCount = ' комната для ';
-  } else if (accommodation.offer.rooms <= 4) {
-    textRoomsCount = ' комнаты для ';
-  } else {
-    textRoomsCount = ' комнат для ';
-  }
-  return accommodation.offer.rooms + textRoomsCount;
-};
+// var getRoomsCount = function (accommodation) {
+//   if (accommodation.offer.rooms === 1) {
+//     var textRoomsCount = ' комната для ';
+//   } else if (accommodation.offer.rooms <= 4) {
+//     textRoomsCount = ' комнаты для ';
+//   } else {
+//     textRoomsCount = ' комнат для ';
+//   }
+//   return accommodation.offer.rooms + textRoomsCount;
+// };
 
-var getGuestsCount = function (accommodation) {
-  if (accommodation.offer.guests === 1) {
-    var textGuestsCount = ' гостя';
-  } else {
-    textGuestsCount = ' гостей ';
-  }
-  return accommodation.offer.guests + textGuestsCount;
-};
+// var getGuestsCount = function (accommodation) {
+//   if (accommodation.offer.guests === 1) {
+//     var textGuestsCount = ' гостя';
+//   } else {
+//     textGuestsCount = ' гостей ';
+//   }
+//   return accommodation.offer.guests + textGuestsCount;
+// };
 
-var getCapacity = function (accommodation) {
-  return getRoomsCount(accommodation) + ' ' + getGuestsCount(accommodation);
-};
+// var getCapacity = function (accommodation) {
+//   return getRoomsCount(accommodation) + ' ' + getGuestsCount(accommodation);
+// };
 
-var getFeatures = function (accommodation, cardFeatures) {
-  for (var i = 0; i < cardFeatures.length; i++) {
-    var count = 0;
-    for (var j = 0; j < accommodation.offer.features.length; j++) {
-      if (cardFeatures[i].classList.contains('popup__feature--' + accommodation.offer.features[j])) {
-        count++;
-      }
-    }
-    if (count === 0) {
-      var featureClasses = cardFeatures[i].classList;
-      for (var q = 0; q < featureClasses.length; q++) {
-        cardFeatures[i].classList.remove(featureClasses[q]);
-      }
-      cardFeatures[i].classList.add('hidden');
-    }
-  }
-};
+// var getFeatures = function (accommodation, cardFeatures) {
+//   for (var i = 0; i < cardFeatures.length; i++) {
+//     var count = 0;
+//     for (var j = 0; j < accommodation.offer.features.length; j++) {
+//       if (cardFeatures[i].classList.contains('popup__feature--' + accommodation.offer.features[j])) {
+//         count++;
+//       }
+//     }
+//     if (count === 0) {
+//       var featureClasses = cardFeatures[i].classList;
+//       for (var q = 0; q < featureClasses.length; q++) {
+//         cardFeatures[i].classList.remove(featureClasses[q]);
+//       }
+//       cardFeatures[i].classList.add('hidden');
+//     }
+//   }
+// };
 
-var getTime = function (accommodation) {
-  return 'Заезд после ' + accommodation.offer.checkin + ', выезд до ' + accommodation.offer.checkout;
-};
+// var getTime = function (accommodation) {
+//   return 'Заезд после ' + accommodation.offer.checkin + ', выезд до ' + accommodation.offer.checkout;
+// };
 
-var getPhotos = function (accommodation, cardPhotoList, cardPhotos) {
-  if (cardPhotos.length < accommodation.offer.photos.length) {
-    var photoTemplate = cardPhotos[0].cloneNode(true);
-    for (var i = 0; i < accommodation.offer.photos.length; i++) {
-      cardPhotoList.appendChild(photoTemplate);
-    }
-  }
-  for (i = 0; i < cardPhotos.length; i++) {
-    cardPhotos[i].src = accommodation.offer.photos[i];
-  }
-};
+// var getPhotos = function (accommodation, cardPhotoList, cardPhotos) {
+//   if (cardPhotos.length < accommodation.offer.photos.length) {
+//     var photoTemplate = cardPhotos[0].cloneNode(true);
+//     for (var i = 0; i < accommodation.offer.photos.length; i++) {
+//       cardPhotoList.appendChild(photoTemplate);
+//     }
+//   }
+//   for (i = 0; i < cardPhotos.length; i++) {
+//     cardPhotos[i].src = accommodation.offer.photos[i];
+//   }
+// };
 
-pinList.appendChild(getPinList(accommodations));
+// pinList.appendChild(getPinList(accommodations));
 
-var mapFilterContainer = document.querySelector('.map__filters-container');
-var cardTemplate = document.querySelector('#card')
-  .content
-  .querySelector('.map__card');
+// var mapFilterContainer = document.querySelector('.map__filters-container');
+// var cardTemplate = document.querySelector('#card')
+//   .content
+//   .querySelector('.map__card');
 
-var renderCard = function (accommodation) {
-  var cardElement = cardTemplate.cloneNode(true);
+// var renderCard = function (accommodation) {
+//   var cardElement = cardTemplate.cloneNode(true);
 
-  var cardTitle = cardElement.querySelector('.popup__title');
-  cardTitle.textContent = accommodation.offer.title;
+//   var cardTitle = cardElement.querySelector('.popup__title');
+//   cardTitle.textContent = accommodation.offer.title;
 
-  var cardAddress = cardElement.querySelector('.popup__text--address');
-  cardAddress.textContent = accommodation.offer.address;
+//   var cardAddress = cardElement.querySelector('.popup__text--address');
+//   cardAddress.textContent = accommodation.offer.address;
 
-  var cardPrice = cardElement.querySelector('.popup__text--price');
-  cardPrice.textContent = accommodation.offer.price + '₽/ночь';
+//   var cardPrice = cardElement.querySelector('.popup__text--price');
+//   cardPrice.textContent = accommodation.offer.price + '₽/ночь';
 
-  var cardType = cardElement.querySelector('.popup__type');
-  cardType.textContent = getOfferType(accommodation);
+//   var cardType = cardElement.querySelector('.popup__type');
+//   cardType.textContent = getOfferType(accommodation);
 
-  var cardCapacity = cardElement.querySelector('.popup__text--capacity');
-  cardCapacity.textContent = getCapacity(accommodation);
+//   var cardCapacity = cardElement.querySelector('.popup__text--capacity');
+//   cardCapacity.textContent = getCapacity(accommodation);
 
-  var cardTime = cardElement.querySelector('.popup__text--time');
-  cardTime.textContent = getTime(accommodation);
+//   var cardTime = cardElement.querySelector('.popup__text--time');
+//   cardTime.textContent = getTime(accommodation);
 
-  var cardFeatureList = cardElement.querySelector('.popup__features');
-  var cardFeatures = cardFeatureList.children;
-  getFeatures(accommodation, cardFeatures);
+//   var cardFeatureList = cardElement.querySelector('.popup__features');
+//   var cardFeatures = cardFeatureList.children;
+//   getFeatures(accommodation, cardFeatures);
 
-  var cardDescription = cardElement.querySelector('.popup__description');
-  cardDescription.textContent = accommodation.offer.description;
+//   var cardDescription = cardElement.querySelector('.popup__description');
+//   cardDescription.textContent = accommodation.offer.description;
 
 
-  var cardPhotoList = cardElement.querySelector('.popup__photos');
-  var cardPhotos = cardPhotoList.children;
-  getPhotos(accommodation, cardPhotoList, cardPhotos);
+//   var cardPhotoList = cardElement.querySelector('.popup__photos');
+//   var cardPhotos = cardPhotoList.children;
+//   getPhotos(accommodation, cardPhotoList, cardPhotos);
 
-  return cardElement;
-};
+//   return cardElement;
+// };
 
-var getCard = function (offer) {
-  var fragment = document.createDocumentFragment();
+// var getCard = function (offer) {
+//   var fragment = document.createDocumentFragment();
 
-  fragment.appendChild(renderCard(offer));
+//   fragment.appendChild(renderCard(offer));
 
-  return fragment;
-};
+//   return fragment;
+// };
 
-mapFilterContainer.before(getCard(accommodations[0]));
+// mapFilterContainer.before(getCard(accommodations[0]));
 
