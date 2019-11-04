4f2cb6f17e898c0e5c5c516519c9b91afc44231e
ilataleksey <ilataleksey@gmail.com>
2019-11-04 11:14:31 +0300

    Добавляет функцию деактивации карты после успешной отправки формы

--- js/map.js
+++ js/map.js
@@ -4,30 +4,6 @@
   var mapFilter = document.querySelector('.map__filters');
   var mapFilterSelects = mapFilter.querySelectorAll('select');
 
-  window.helpers.setAttribute(mapFilterSelects, 'disabled');
-  window.helpers.setAttribute(window.form.adFormInputs, 'disabled');
-  window.helpers.setAttribute(window.form.adFormSelects, 'disabled');
-  window.helpers.setAttribute(window.form.adFormTextarea, 'disabled');
-  window.helpers.setAttribute(window.form.adFormButton, 'disabled');
-
-  var successeLoadHandler = function (accommodations) {
-    var fragment = document.createDocumentFragment();
-    for (var i = 0; i < accommodations.length; i++) {
-      fragment.appendChild(window.pin.renderPin(accommodations[i]));
-    }
-    window.pin.pinList.appendChild(fragment);
-  };
-
-  var errorHandler = function (errorMessage) {
-    var errorTemplate = document.querySelector('#error')
-      .content
-      .querySelector('.error');
-    var errorElement = errorTemplate.cloneNode(true);
-    errorElement.querySelector('.error__message').textContent = errorMessage;
-    document.body.insertAdjacentElement('afterbegin', errorElement);
-
-  };
-
   window.map = {
     map: document.querySelector('.map'),
     mapFilterContainer: document.querySelector('.map__filters-container'),
@@ -37,10 +13,23 @@
       window.helpers.removeAttribute(mapFilterSelects, 'disabled');
       window.helpers.removeAttribute(window.form.adFormInputs, 'disabled');
       window.helpers.removeAttribute(window.form.adFormSelects, 'disabled');
-      window.helpers.removeAttribute(window.form.adFormTextarea, 'disabled');
-      window.helpers.removeAttribute(window.form.adFormButton, 'disabled');
+      window.helpers.removeAttribute(window.form.adFormTextareas, 'disabled');
+      window.helpers.removeAttribute(window.form.adFormButtons, 'disabled');
       window.pin.getAddress();
-      window.backend.load(window.backend.URL, successeLoadHandler, errorHandler);
+      window.backend.load(window.backend.URL, window.backend.successLoadHandler, window.backend.errorHandler);
+    },
+
+    mapDeactivation: function () {
+      window.map.map.classList.add('map--faded');
+      window.helpers.setAttribute(mapFilterSelects, 'disabled');
+      window.helpers.setAttribute(window.form.adFormInputs, 'disabled');
+      window.helpers.setAttribute(window.form.adFormSelects, 'disabled');
+      window.helpers.setAttribute(window.form.adFormTextareas, 'disabled');
+      window.helpers.setAttribute(window.form.adFormButtons, 'disabled');
+      window.form.getDefaultAddress();
     }
   };
+
+  window.map.mapDeactivation();
+
 })();
