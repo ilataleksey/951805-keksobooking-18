10bac242bc32dad671f6565ba8aeeaa75671d313
ilataleksey <ilataleksey@gmail.com>
2019-11-09 17:43:15 +0300

    Добавляет скрипт сортировки меток

--- js/map.js
+++ js/map.js
@@ -4,6 +4,39 @@
   var mapFilter = document.querySelector('.map__filters');
   var mapFilterSelects = mapFilter.querySelectorAll('select');
 
+  var accommodations = [];
+  var successLoadHandler = function (data) {
+    accommodations = data;
+    window.pin.renderPins(accommodations);
+  };
+
+  var newAccommodations = [];
+  var housingType;
+
+  var filterHousingType = document.querySelector('#housing-type');
+  filterHousingType.addEventListener('change', function (evt) {
+    evt.preventDefault();
+    window.pin.deleteChildren(window.map.mapFilterContainer.children, 1);
+    var newType = filterHousingType.value;
+    onFilterHousingTypeChange(newType);
+  });
+
+  var updatePins = function () {
+    if (housingType === 'any') {
+      newAccommodations = accommodations;
+    } else {
+      newAccommodations = accommodations.filter(function (accommodation) {
+        return accommodation.offer.type === housingType;
+      });
+    }
+    window.pin.renderPins(newAccommodations);
+  };
+
+  var onFilterHousingTypeChange = window.debounce(function (type) {
+    housingType = type;
+    updatePins();
+  });
+
   window.map = {
     map: document.querySelector('.map'),
     mapFilterContainer: document.querySelector('.map__filters-container'),
@@ -16,7 +49,7 @@
       window.helpers.removeAttribute(window.form.adFormTextareas, 'disabled');
       window.helpers.removeAttribute(window.form.adFormButtons, 'disabled');
       window.pin.getAddress();
-      window.backend.load(window.backend.URL, window.backend.successLoadHandler, window.backend.errorHandler);
+      window.backend.load(window.backend.URL, successLoadHandler, window.backend.errorHandler);
     },
 
     onFormSend: function () {
